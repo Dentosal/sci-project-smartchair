34ddbd47a05b126ff2ad18eab5312cfeb2aab174
Hannes Karppila <hannes.karppila@gmail.com>
2019-03-25 10:27:04 +0200

    Implement device autodiscovery on macos

--- read_to_db.py
+++ read_to_db.py
@@ -6,19 +6,24 @@ import time
 enva = os.environ.get("ARDUINO")
 
 if enva is None:
-    devs = [d for d in os.listdir("/dev") if d.startswith("ttyACM")]
+    devs = [
+        d
+        for d in os.listdir("/dev")
+        if d.startswith("ttyACM") or d.startswith("cu.usbmodem")
+    ]
     assert len(devs) == 1, "Incorrect number of devices found"
 else:
     devs = [enva]
- 
+
 s = serial.Serial("/dev/" + devs[0])
- 
- 
+
 con = sqlite3.connect("h.db")
 cur = con.cursor()
- 
-cur.execute("CREATE TABLE IF NOT EXISTS History(timestamp INTEGER PRIMARY KEY, value INTEGER)")
- 
+
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS History(timestamp INTEGER PRIMARY KEY, value INTEGER)"
+)
+
 nt = (time.time()) + 1
 while True:
     b0 = int.from_bytes(s.read(), "little")
@@ -30,5 +35,7 @@ while True:
 
     print(bin(b0)[2:].zfill(8))
 
-    cur.execute("INSERT INTO History (timestamp, value) VALUES (?, ?)", (int(time.time()), b0))
+    cur.execute(
+        "INSERT INTO History (timestamp, value) VALUES (?, ?)", (int(time.time()), b0)
+    )
     con.commit()
